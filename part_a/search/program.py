255ed6b9fd33921f729cf55ad7613a28f3222351
Yijun Guo <yijguo1@student.unimelb.edu.au>
2023-03-20 21:26:43 +1100

    spread initial version

--- part_a/search/program.py
+++ part_a/search/program.py
@@ -3,14 +3,41 @@
 
 from .utils import render_board
 
-def spread(tup1, tup2):
-    power = tup1[3]
-    action_list = []
+board = {(6, 3): ("b", 5), (5, 3): ("b", 1), (4, 3): ("r", 1), (3, 3): ("r", 6), (6, 4): ("b", 1),
+          (6, 5): ("b", 1), (6, 2): ("r", 1)}
+tuple1 = (6, 3, "b", 5)
+tuple2 = (0, 1)
+
+def spread(tup1: tuple, tup2: tuple, board: dict[tuple, tuple]):
+    power = tup1[3]    # power of the original cell
+    coordinates_list = []   # list to store coordinates after spread
+
     i = 1
     while i <= power:
-        action_list.append(((tup1[0]+i*tup2[0])%7, (tup1[1]+i*tup2[1])%7))
-        i+=1
-    # 查找坐标是否有其他棋子
+        # find the coordinates after spread
+        coordinates_list.append(((tup1[0]+i*tup2[0]) % 7, (tup1[1]+i*tup2[1]) % 7))
+        i += 1
+
+    spread_lists = []
+    # find the power of each cell after spread
+    for coordinate in coordinates_list:
+        if coordinate in board:
+            color, power = board[coordinate]
+            if power < 6:
+                new_power = power + 1
+
+            # if the original power is 6, the cell become empty
+            else:
+                new_power = 0
+        # when the spread position is empty
+        else:
+            new_power = 1
+        if new_power != 0:
+            spread_lists.append((coordinate[0], coordinate[1], tup1[2], new_power))
+
+    return spread_lists
+
+
 
 def search(input: dict[tuple, tuple]) -> list[tuple]:
     """
@@ -26,7 +53,7 @@ def search(input: dict[tuple, tuple]) -> list[tuple]:
     # board state in a human-readable format. Try changing the ansi argument 
     # to True to see a colour-coded version (if your terminal supports it).
     print(render_board(input, ansi=False))
-    #sdf
+    print(spread(tuple1, tuple2, board))
 
     # Here we're returning "hardcoded" actions for the given test.csv file.
     # Of course, you'll need to replace this with an actual solution...
